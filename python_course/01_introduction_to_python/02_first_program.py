0fface5cfba815270dcab2b4b417598abf170fb2
Vishwas sharma <svishwas880@gmail.com>
2026-02-19 15:04:57 +0530

    Create 02_first_program.py

---
+++ python_course/01_introduction_to_python/02_first_program.py
@@ -0,0 +1,42 @@
+Running First Python Program (Hello World)
+What is a Hello World Program?
+
+A Hello World program is the simplest program used to test whether Python is installed correctly and working properly. It prints a simple message on the screen.
+
+Steps to Run Your First Python Program
+Step 1: Install Python
+
+Download Python from the official website:
+https://www.python.org
+
+While installing, make sure to check "Add Python to PATH".
+
+Step 2: Open an Editor
+
+You can use:
+
+VS Code
+
+Notepad
+
+PyCharm
+
+For beginners, VS Code is recommended.
+
+Step 3: Create a Python File
+
+Create a new file and save it as:
+
+hello.py
+
+Step 4: Write the Code
+print("Hello, World!")
+
+Step 5: Run the Program
+
+Open terminal or command prompt in the same folder and type:
+
+python hello.py
+
+Step 6: Output
+Hello, World!
