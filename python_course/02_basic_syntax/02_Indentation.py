26f5ed3a57e5b5fac1f0676e383b15e7544fd0f6
Vishwas sharma <svishwas880@gmail.com>
2026-02-19 15:07:10 +0530

    Create 02_Indentation.py

---
+++ python_course/02_basic_syntax/02_Indentation.py
@@ -0,0 +1,29 @@
+
+Indentation in Python:-
+What is Indentation?
+
+Indentation refers to the spaces at the beginning of a line of code.
+In Python, indentation is very important because it defines the block of code.
+
+Unlike other programming languages that use { }, Python uses indentation to group statements.
+
+Why Indentation is Important
+
+Defines blocks of code
+
+Improves readability
+
+Required in loops, functions, and conditions
+
+Python will give an error if indentation is incorrect
+
+Example Without Indentation (Error)
+if True:
+print("Hello")
+
+
+This will give an IndentationError because the print statement is not indented.
+
+Correct Example
+if True:
+    print("Hello")
