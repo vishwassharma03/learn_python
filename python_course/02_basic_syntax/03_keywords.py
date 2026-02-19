0bada53bbda6515e02b9fe10e14844746fcf61dd
Vishwas sharma <svishwas880@gmail.com>
2026-02-19 15:07:40 +0530

    Create 03_keywords.py

---
+++ python_course/02_basic_syntax/03_keywords.py
@@ -0,0 +1,23 @@
+Keywords in Python:-
+
+What are Keywords?
+
+Keywords are reserved words in Python that have a special meaning and cannot be used as variable names, function names, or identifiers.
+
+Python uses keywords to define the syntax and structure of the programming language.
+
+List of Python Keywords
+
+Some commonly used Python keywords are:
+
+False   await   else   import   pass
+None    break   except  in      raise
+True    class   finally is      return
+and     continue for    lambda  try
+as      def      from   nonlocal while
+assert  del      global not     with
+async   elif     if     or      yield
+
+Example of Keywords Usage
+if True:
+    print("Hello Python")
