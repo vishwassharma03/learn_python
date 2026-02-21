3db32dc80b93d13625358039754755c48d194439
Vishwas sharma <svishwas880@gmail.com>
2026-02-21 07:47:29 +0530

    Create 01_Variables_introduction.py

---
+++ python_course/03_Variables and Data Types/01_Variables_introduction.py
@@ -0,0 +1,30 @@
+Variables in Python
+What is a Variable?
+
+A variable is a container used to store data in memory.
+It allows us to store values and use them later in a program.
+
+Example:
+
+name = "vishwas"
+age = 21
+
+
+Here:
+
+name is a variable storing a string
+
+age is a variable storing an integer
+
+How Variables Work in Python
+
+Python automatically decides the data type of a variable based on the value assigned.
+
+Example:
+
+x = 10        # integer
+y = 3.14      # float
+z = "Hello"   # string
+
+
+This is called dynamic typing.
