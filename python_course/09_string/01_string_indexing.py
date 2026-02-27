d14a0a12ad39fb8b3c9597234d9915f9d3a7ef13
Vishwas sharma <svishwas880@gmail.com>
2026-02-27 23:03:52 +0530

    Create 01_string_indexing.py

---
+++ python_course/09_string/01_string_indexing.py
@@ -0,0 +1,24 @@
+Strings in Python
+What is a String?
+
+A string is a sequence of characters enclosed in single (' '), double (" "), or triple quotes (''' ''' / """ """).
+
+Strings are used to store text data.
+
+Creating Strings
+s1 = "Hello"
+s2 = 'Python'
+s3 = """Welcome to Python"""
+String Indexing
+
+Each character has an index starting from 0.
+
+text = "Python"
+
+print(text[0])   # P
+print(text[3])   # h
+Negative Indexing
+text = "Python"
+
+print(text[-1])  # n
+print(text[-2])  # o
