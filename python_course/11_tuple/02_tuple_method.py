45e4cdf0a32026c9d1a178e2c994e90f828dcd70
Vishwas sharma <svishwas880@gmail.com>
2026-03-02 13:49:21 +0530

    Create 02_tuple_method.py

---
+++ python_course/11_tuple/02_tuple_method.py
@@ -0,0 +1,29 @@
+Tuple Methods in Python
+What are Tuple Methods?
+
+Tuples are immutable, so they have very few methods compared to lists.
+
+Python provides only two built-in tuple methods.
+
+1️⃣ count()
+
+Used to count how many times an element appears in a tuple.
+
+t = (1, 2, 2, 3, 2)
+
+print(t.count(2))
+
+Output
+
+3
+2️⃣ index()
+
+Used to find the index position of an element.
+
+t = (10, 20, 30, 40)
+
+print(t.index(30))
+
+Output
+
+2
